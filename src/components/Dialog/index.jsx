f797cbec29d264d1af4eed4178db30c4dc62ee1e
danilonishimoto <daniyuzo.nishimoto@gmail.com>
2026-04-12 13:16:01 -0300

    feat: dialog component

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,38 @@
+import { useRef, useEffect } from "react";
+import "./dialog.style.css";
+import { IconClose } from "../icons";
+
+export function Dialog({ isOpen, onClose, children }) {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      openDialog();
+    } else {
+      closeDialog();
+    }
+  }, [isOpen]);
+
+  const openDialog = () => {
+    dialogRef.current.showModal();
+  };
+
+  const closeDialog = () => {
+    dialogRef.current.close();
+  };
+
+  return (
+    <>
+      <dialog ref={dialogRef} className="dialog">
+        <div className="btn-close-wrapper">
+          <button autoFocus onClick={onClose}
+          className="btn-close"
+          >
+            <IconClose />
+          </button>
+        </div>
+        {children}
+      </dialog>
+    </>
+  );
+}
